7e51bb8af790ae001369d23a9000bf0a7f63c69f
DavidGarcia10-D <gggarciadiaz@gmail.com>
2026-04-28 08:32:01 -0500

    cambios burbuja al carro e incurcion de pago form en carrito6676

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,8 @@ const app = express();
 const allowedOrigins = [
   "http://localhost:3000",
   "http://localhost:5000",
-  "https://wish-market-place-front.onrender.com"
+  "https://wish-market-place-front.onrender.com",
+  "https://wish-market-place-mercado-de-deseos-rjk7t9tw2.vercel.app/"
 ];
 
 app.use(cors({
