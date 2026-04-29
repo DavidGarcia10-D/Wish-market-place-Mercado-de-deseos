1c126f2e2f2d931e84817736e8191772f2c8afb1
DavidGarcia10-D <gggarciadiaz@gmail.com>
2026-04-29 17:26:43 -0500

    documentos legales1

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -47,7 +47,7 @@ const Footer = ({ modoOscuro }) => {
         <div style={{ display: "flex", justifyContent: "center", gap: "15px", flexWrap: "wrap" }}>
           <img src="https://images.seeklogo.com/logo-png/43/1/pse-logo-png_seeklogo-433463.png" alt="PSE" style={{ height: "35px" }} />
           <img src="https://img.icons8.com/?size=50&id=1429&format=png" alt="Visa" style={{ height: "35px" }} />
-          <img src="https://www.mastercard.com/brandcenter/us/en/home/_jcr_content/root/container/container_1578756628/container_1860858030/container/teaser4_copy_2821464.coreimg.png/1751029673412/mastercard-symbol-square-black.png" style={{ height: "35px" }} />
+          <img src="https://www.mastercard.com/brandcenter/us/en/home/_jcr_content/root/container/container_1578756628/container_1860858030/container/teaser4_copy_2821464.coreimg.png/1751029673412/mastercard-symbol-square-black.png" alt="Mastercard" style={{ height: "35px" }} />
           <img src="https://upload.wikimedia.org/wikipedia/commons/f/fa/American_Express_logo_%282018%29.svg" alt="American Express" style={{ height: "35px" }} />
         </div>
       </div>
@@ -56,7 +56,7 @@ const Footer = ({ modoOscuro }) => {
       <div style={{ marginBottom: "0.5rem" }}>
         <a href="/privacidad" style={{ color: colorLink, margin: "0 10px", textDecoration: "none" }}>Política de Privacidad</a>
         <a href="/terminos" style={{ color: colorLink, margin: "0 10px", textDecoration: "none" }}>Términos y Condiciones</a>
-        <a href="/reembolso" style={{ color: colorLink, margin: "0 10px", textDecoration: "none" }}>Política de Devoluciones</a>
+        <a href="/reembolso" style={{ color: colorLink, margin: "0 10px", textDecoration: "none" }}>Política de Reembolso</a>
       </div>
 
       <p>© 2026 Wish Marketplace – Mercado de Deseos</p>
